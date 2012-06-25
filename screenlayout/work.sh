555356177493bcbd4d1f199328151084ceae5609
Yuri Prezument <y@yprez.com>
2012-06-25 15:49:05 +0300

    Screen positions

--- screenlayout/work.sh
+++ screenlayout/work.sh
@@ -2,8 +2,8 @@
 xrandr --output DP3 --off \
 	--output DP2 --off \
 	--output DP1 --off \
-	--output HDMI3 --mode 1920x1200 --primary --pos 1920x0 --rotate normal \
+	--output HDMI3 --mode 1920x1200 --primary --pos 1366x0 --rotate normal \
 	--output HDMI2 --off \
 	--output HDMI1 --off \
-	--output LVDS1 --mode 1366x768 --noprimary --pos 0x132 --rotate normal \
+	--output LVDS1 --mode 1366x768 --noprimary --pos 0x432 --rotate normal \
 	--output VGA1 --off
